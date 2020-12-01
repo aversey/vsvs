965790f4f600790cf68a84ec052426abc39c69c8
Aleksey Veresov <aleksey@veresov.pro>
2020-12-01 23:10:17 +0300

    .

---
+++ vsvs.sh
@@ -0,0 +1,20 @@
+res=""
+met=false
+while read line
+do
+    value=$(echo -n "$line" | sed 's/ .*//')
+    if [ "${line:0:1}" == " " ]
+    then
+        if [ $met == true ]
+        then
+            res="$res\n$value"
+        fi
+    else
+        if [ $(echo -n "$line" | sed 's/[^ ]* //') == $1 ]
+        then
+            met=true
+            res=value
+        fi
+    fi
+done
+echo -n $res
