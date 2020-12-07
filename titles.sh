8bb86acc75a348599c706c89715d71b2eae6bed0
Aleksey Veresov <aleksey@veresov.pro>
2020-12-07 09:53:49 +0300

    .

--- titles.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-export base="$(pwd)/"
-export proc=$$
-mkdir -p "$base/proc/$proc"
-vsvs-extract $'echo "$value" | vsvs-parse \'test "$key" == "title" && echo "$value"\'' data
-rm -r "$base/proc/$proc"
